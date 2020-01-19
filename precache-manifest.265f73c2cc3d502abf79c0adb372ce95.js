d9de422da013c041f92a8096f06a60b6147a652a
Kushal Yadav <kushaly1996@gmail.com>
2020-01-19 12:58:39 +0530

    Updates

---
+++ precache-manifest.265f73c2cc3d502abf79c0adb372ce95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30fd1b7f689722295c92e39103f6e44",
+    "url": "/kps-app/index.html"
+  },
+  {
+    "revision": "5558e5ebac5067383e56",
+    "url": "/kps-app/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "fa857e1d4356d5b539ad",
+    "url": "/kps-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5558e5ebac5067383e56",
+    "url": "/kps-app/static/js/2.374c9d7b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/kps-app/static/js/2.374c9d7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa857e1d4356d5b539ad",
+    "url": "/kps-app/static/js/main.4f4ca388.chunk.js"
+  },
+  {
+    "revision": "77a363c09b0e657fa63f",
+    "url": "/kps-app/static/js/runtime-main.25b4984a.js"
+  }
+]);
